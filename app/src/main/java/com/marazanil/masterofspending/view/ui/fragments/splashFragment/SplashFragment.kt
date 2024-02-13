c14fa5cd6b5ae072382e2aeaf0e734fbd8b2a2d7
Ahmet Anıl Demiryürek <79675017+ahmetanildemiryurek@users.noreply.github.com>
2024-02-13 16:33:54 +0300

    Splash screen will now work with threads

--- app/src/main/java/com/marazanil/masterofspending/view/ui/fragments/splashFragment/SplashFragment.kt
+++ app/src/main/java/com/marazanil/masterofspending/view/ui/fragments/splashFragment/SplashFragment.kt
@@ -8,12 +8,23 @@ import androidx.navigation.fragment.findNavController
 import com.marazanil.masterofspending.R
 
 class SplashFragment : Fragment(R.layout.fragment_splash) {
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val button: Button = view.findViewById(R.id.appLoginButton)
-        button.setOnClickListener {
-            findNavController().navigate(R.id.action_splashFragment_to_expenseListFragment)
+        val splash = object : Thread() {
+            override fun run() {
+                try {
+                    //thread 5000 ms yani 5 saniye beklesin
+                    Thread.sleep(5000)
+                    //fragment.navigate ile splash ekranından sonra MainActivity ekranı açılsın diyoruz
+                    findNavController().navigate(R.id.action_splashFragment_to_expenseListFragment)
+                }catch (e : Exception){
+                    e.printStackTrace()
+                }
+            }
         }
+        splash.start()
     }
 }
+

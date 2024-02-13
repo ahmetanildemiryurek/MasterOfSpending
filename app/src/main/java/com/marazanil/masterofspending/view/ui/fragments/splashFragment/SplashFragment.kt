734b1b9a015e6aa352141e8ed6fd57fad7bc0a61
Ahmet Anıl Demiryürek <79675017+ahmetanildemiryurek@users.noreply.github.com>
2024-02-13 18:45:03 +0300

    White screen error resolved,(Thread's -> Handler).

--- app/src/main/java/com/marazanil/masterofspending/view/ui/fragments/splashFragment/SplashFragment.kt
+++ app/src/main/java/com/marazanil/masterofspending/view/ui/fragments/splashFragment/SplashFragment.kt
@@ -1,30 +1,20 @@
 package com.marazanil.masterofspending.view.ui.fragments.splashFragment
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.marazanil.masterofspending.R
 
-class SplashFragment : Fragment(R.layout.fragment_splash) {
+class SplashFragment : Fragment(R.layout.fragment_splash) { // Layout dosyasını burada belirttim.
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val splash = object : Thread() {
-            override fun run() {
-                try {
-                    //thread 5000 ms yani 5 saniye beklesin
-                    Thread.sleep(5000)
-                    //fragment.navigate ile splash ekranından sonra MainActivity ekranı açılsın diyoruz
-                    findNavController().navigate(R.id.action_splashFragment_to_expenseListFragment)
-                }catch (e : Exception){
-                    e.printStackTrace()
-                }
-            }
-        }
-        splash.start()
+        Handler(Looper.getMainLooper()).postDelayed({
+            // 5 saniye bekledikten sonra expenseListFragment'e yönlendirme.
+            findNavController().navigate(R.id.action_splashFragment_to_expenseListFragment)
+        }, 5000) // 5 saniye bekletme süresi.
     }
 }
-

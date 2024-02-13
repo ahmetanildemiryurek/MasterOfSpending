090b0b64873c2fc3a96d5118f69e936d2c077a1b
Ahmet Anıl Demiryürek <79675017+ahmetanildemiryurek@users.noreply.github.com>
2024-02-13 06:05:40 +0300

    SplachActivity classı silindi SplashFragment class'ı güncellendi ve geçiş yapısı  fragment ile sağlanmış oldu.

--- app/src/main/java/com/marazanil/masterofspending/view/ui/fragments/splashFragment/SplashFragment.kt
+++ app/src/main/java/com/marazanil/masterofspending/view/ui/fragments/splashFragment/SplashFragment.kt
@@ -1,26 +1,19 @@
 package com.marazanil.masterofspending.view.ui.fragments.splashFragment
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.marazanil.masterofspending.R
 
-class SplashFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_splash, container, false)
-    }
-
+class SplashFragment : Fragment(R.layout.fragment_splash) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val button: Button = view.findViewById(R.id.appLoginButton)
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_splashFragment_to_expenseListFragment)
+        }
     }
-
 }

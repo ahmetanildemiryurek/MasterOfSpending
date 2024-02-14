b68b5dadb570204dbaec88a1369115401ddaa2bb
Ahmet Anıl Demiryürek <79675017+ahmetanildemiryurek@users.noreply.github.com>
2024-02-14 14:47:55 +0300

    Expenditure details were extracted from the database.

--- app/src/main/java/com/marazanil/masterofspending/data/db/service/ExpenseDao.kt
+++ app/src/main/java/com/marazanil/masterofspending/data/db/service/ExpenseDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import com.marazanil.masterofspending.data.db.entity.ExpenseEntity
+import retrofit2.http.GET
 
 @Dao
 interface ExpenseDao {
@@ -14,6 +15,8 @@ interface ExpenseDao {
      fun deleteExpense(expense: ExpenseEntity)
     @Query("SELECT * FROM expenses")
      fun getAllExpenses(): List<ExpenseEntity>
+    @Query("SELECT * FROM expenses WHERE expenseId = :id")
+    fun getExpenseById(id: Int): ExpenseEntity?
 
 
 
--- app/src/main/java/com/marazanil/masterofspending/view/ui/fragments/expenseDetailsFragment/ExpenseDetailsFragment.kt
+++ app/src/main/java/com/marazanil/masterofspending/view/ui/fragments/expenseDetailsFragment/ExpenseDetailsFragment.kt
@@ -5,13 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.marazanil.masterofspending.R
+import com.marazanil.masterofspending.data.db.ExpenseDatabase
+import com.marazanil.masterofspending.data.db.entity.ExpenseEntity
 import com.marazanil.masterofspending.databinding.FragmentExpenseDetailsBinding
 
 
 class ExpenseDetailsFragment : Fragment() {
-
-
     private var _binding: FragmentExpenseDetailsBinding? = null
     private val binding get() = _binding!!
 
@@ -22,11 +21,20 @@ class ExpenseDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val expenseId = arguments?.getInt("expenseId") ?: return
+        val expenseDao = ExpenseDatabase.getDatabase(requireContext()).expenseDao()
+        val expense = expenseDao.getExpenseById(expenseId)
 
-        val expenseId = arguments?.getInt("expenseId")
+        displayExpenseDetails(expense)
+    }
 
-        binding.deleteButton.setOnClickListener {
-
+    private fun displayExpenseDetails(expense: ExpenseEntity?) {
+        expense?.let {
+            binding.expenseDetailsName.text = it.expenseName
+            binding.expenseDetailsPrice.text = it.expensePrice
+            binding.currencyDetailsType.text = it.currencyType
+            binding.expenseDetails.text = it.expenseDetails
+            binding.expenseDetailsNumber.text = it.expenseNumber.toString()
         }
     }
 
